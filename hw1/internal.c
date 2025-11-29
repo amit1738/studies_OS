13f772298d5c09c7c5d9aab8b3bb8200f2c3b717
amit1738 <sharvit.amit@gmail.com>
2025-11-29 19:16:31 +0200

    implement cd command and apply delete of main.c

--- hw1/internal.c
+++ hw1/internal.c
@@ -8,13 +8,23 @@
 //execute internal commands (exit, cd, jobs)
 //returns: 0 = success, -1 = error
 int execute_internal(char **args, int arg_count) {
+    //cd command
     if (strcmp(args[0], "cd") == 0) {
         if (arg_count < 2) {
             fprintf(stderr, "cd: missing argument\n");
             return -1;
-        } else if (chdir(args[1]) != 0) {
-            fprintf(stderr, "cd: %s: %s\n", args[1], strerror(errno)); //TODO: need to understand
-            return -1;
+        } else if (strcmp(args[1], "..") == 0) {    
+            //go up one directory
+            if (chdir("..") != 0) {
+                fprintf(stderr, "cd: %s: %s\n", args[1], strerror(errno));
+                return -1;
+            }
+        } else {
+            //change to specified directory
+            if (chdir(args[1]) != 0) {
+                fprintf(stderr, "cd: %s: %s\n", args[1], strerror(errno));
+                return -1;
+            }
         }
         return 0;
     }
